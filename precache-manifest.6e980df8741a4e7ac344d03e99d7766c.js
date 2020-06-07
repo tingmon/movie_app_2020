c8cf39a0b95fab91255718d1e6d770de93d6d88a
Taekmin Jeong <tingmon2@gmail.com>
2020-06-07 05:48:07 -0400

    Updates

---
+++ precache-manifest.6e980df8741a4e7ac344d03e99d7766c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96074be9b6664157c8a7e51e43d300d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4f2b463943f9c99f4d87",
+    "url": "/movie_app_2020/static/css/main.33c79733.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2b463943f9c99f4d87",
+    "url": "/movie_app_2020/static/js/main.7921a8c9.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
